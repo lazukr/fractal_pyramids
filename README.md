e69b74239bc2420a9e34f44495fdd0f32a06b563
lazukr <peterh10201@gmail.com>
2021-01-25 22:55:00 -0800

    Updated the readme to include the changes made. Update the spacing configuration to be [0, 0] by default. Removed redundant code and magic numbers from fractal.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Fractal Pyramids
 
-I made this for fun to see what a [T-square (fractal)](https://en.wikipedia.org/wiki/T-square_(fractal)) would look like as bases of pyramids. **This strictly uses square branches related by 1/2.**
+I made this for fun to see what a [T-square (fractal)](https://en.wikipedia.org/wiki/T-square_(fractal)) would look like as bases of pyramids. By default, the square branch relation is 1/2. The `factor` configuration allows you to adjust this manually, but it may yield unexpected results.
 
 ## Use
 
@@ -22,7 +22,9 @@ There is a `config.yml` file that comes with some options:
 
 | Option | Type | Description |
 | - | - | - |
-| Minimum_size | +Integer | The minimum length for half length/width of the pyramid base. Thus, a value of `1` will make it so the smallest pyramid base is `2` in length/width.
-Tile_size | +Integer | The size of the tile. This purely scales the whole pyramid up.
-Iterations | +Integer | Number of times the fractal pattern should occur. This indirectly decides the maximium pyramid base as it is just `Minimum_size * 2 ^ Iterations`.
-Ignore_proper_fractal | Boolean | While developing, I did not account for the fractal pattern properly and always did the fractal pattern in all 4 corners. If this is `true`, it will always draw all 4 corners.
+| minimum_size | +int | The minimum length for half length/width of the pyramid base. Thus, a value of `1` will make it so the smallest pyramid base is `2` in length/width.
+tile_size | +int | The size of the tile. This purely scales the whole pyramid up.
+iterations | +int | Number of times the fractal pattern should occur. This indirectly decides the maximium pyramid base as it is just `Minimum_size * 2 ^ Iterations`.
+ignore_proper_fractal | bool | While developing, I did not account for the fractal pattern properly and always did the fractal pattern in all 4 corners. If this is `true`, it will always draw all 4 corners.
+factor | +int | This is the growth factor of the pyramids. It's best to keep this at 2 as anything else seems to lead to weird behaviour (either limitations of the drawing or I did something wrong).
+spacing | [+int, +int] | This provides a spacing per each set of iteration using the following formula: `spacing[0] * x + spacing[1]` where x is the current iteration.
--- fractal.py
+++ fractal.py
@@ -1,6 +1,10 @@
 from typing import List
 from utils import Square
-from PIL import Image, ImageDraw
+
+TOP_LEFT = 0
+TOP_RIGHT = 1
+BOTTOM_LEFT = 10
+BOTTOM_RIGHT = 11
 
 def recursive_half_fractal(size: int, factor: int, iterations: int, x: int, y: int, parent_direction: int, ignore_parent_directon: bool, spacing: List[int], accumulated_list: List[Square]) -> List[Square]:
     square = Square(size, x, y)
@@ -19,16 +23,16 @@ def recursive_half_fractal(size: int, factor: int, iterations: int, x: int, y: i
 
         half_next_size = size // factor + spacing[iterations]
         # top left
-        if (parent_direction != 0 or ignore_parent_directon == True):
-            accumulated_list + recursive_half_fractal(size, factor, iterations, left - half_next_size, top - half_next_size, 11, ignore_parent_directon, spacing, accumulated_list)
+        if (parent_direction != TOP_LEFT or ignore_parent_directon):
+            accumulated_list + recursive_half_fractal(size, factor, iterations, left - half_next_size, top - half_next_size, BOTTOM_RIGHT, ignore_parent_directon, spacing, accumulated_list)
         # top right
-        if (parent_direction != 1 or ignore_parent_directon == True):
-            accumulated_list + recursive_half_fractal(size, factor, iterations, right + half_next_size, top - half_next_size, 10, ignore_parent_directon, spacing, accumulated_list)
+        if (parent_direction != TOP_RIGHT or ignore_parent_directon):
+            accumulated_list + recursive_half_fractal(size, factor, iterations, right + half_next_size, top - half_next_size, BOTTOM_LEFT, ignore_parent_directon, spacing, accumulated_list)
         # bottom left
-        if (parent_direction != 10 or ignore_parent_directon == True):
-            accumulated_list + recursive_half_fractal(size, factor, iterations, left - half_next_size, bottom + half_next_size, 1, ignore_parent_directon, spacing, accumulated_list)
+        if (parent_direction != BOTTOM_LEFT or ignore_parent_directon):
+            accumulated_list + recursive_half_fractal(size, factor, iterations, left - half_next_size, bottom + half_next_size, TOP_RIGHT, ignore_parent_directon, spacing, accumulated_list)
         # bottom right
-        if (parent_direction != 11 or ignore_parent_directon == True):
-            accumulated_list + recursive_half_fractal(size, factor, iterations, right + half_next_size, bottom + half_next_size, 0, ignore_parent_directon, spacing, accumulated_list)
+        if (parent_direction != BOTTOM_RIGHT or ignore_parent_directon):
+            accumulated_list + recursive_half_fractal(size, factor, iterations, right + half_next_size, bottom + half_next_size, TOP_LEFT, ignore_parent_directon, spacing, accumulated_list)
         
         return accumulated_list
